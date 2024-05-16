6d143d8823a1a0eaf9a65d6e150824a446673e5b
Prabodashan <prabodashan14@gmail.com>
2024-05-16 22:29:10 +0530

    Added next auth to signin and signout using google

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "@/assets/styles/globals.css";
 
 import Navbar from "@/components/sections/Navbar";
 import Footer from "@/components/sections/Footer";
+import AuthProvider from "@/components/sections/AuthProvider";
 
 export const metadata = {
   title: "Add Manager |Find The Perfect",
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
